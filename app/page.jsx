e671a7dec66c8affb238a548dd26cfd4003bbf14
“[halil <halilprofil@gmail.com>
2024-10-10 17:00:11 +0300

    action dosyası oluşturuldu

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,29 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { login, signup } from "@actions/action"
 
 export default function Home() {
   return (
     <>
-     <>login</>
+     <form className="loginForm">
+       <div>
+          <label htmlFor="fullName">Name</label>
+          <input id="fullName" name="fullName" type="text" required />
+       </div>
+
+       <div>
+          <label htmlFor="email">Email</label>
+          <input id="email" name="email" type="email" required />
+      </div>
+
+      <div>
+          <label htmlFor="password">Password</label>
+          <input id="password" name="password" type="password" required />
+      </div>
+
+      <button formAction={login}>Log in</button>
+      <button formAction={signup}>Sign up</button>
+    </form>
     </>
   );
 }
