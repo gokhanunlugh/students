53d4c48a149b3a5634449b30273f5e953362ef08
Gökhan Ünlü <gokhanunlu94@gmail.com>
2024-10-10 22:58:22 +0300

    action importlando

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,6 +1,7 @@
 
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from 'next/navigation'
+import { addStudent } from './action'
 
 export default async function AdminPage(){
     const supabase = createClient("https://edwrpskmitwxvipmcyoy.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVkd3Jwc2ttaXR3eHZpcG1jeW95Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyODU2NjUzNCwiZXhwIjoyMDQ0MTQyNTM0fQ.0gHmdxie-I7hLOMRDhoxjQNYSoN4u9WnSdWjQhm3nPI");
@@ -22,6 +23,7 @@ export default async function AdminPage(){
             <input type="number" name="sinav1" placeholder="Sınav1" />
             <input type="number" name="sinav2" placeholder="Sınav2" />
             <input type="number" name="sinav3" placeholder="Sınav3" />
+            <button>Ekle</button>
             
 
         
