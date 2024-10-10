f2f532722d614e0b91d4fea26667979840d5ce92
Gökhan Ünlü <gokhanunlu94@gmail.com>
2024-10-10 23:27:09 +0300

    action yapıldı

--- app/admin/action.js
+++ app/admin/action.js
@@ -1,3 +1,4 @@
+"use server"
 import { defaultHeader } from "@/utils/header";
 
 
@@ -7,17 +8,22 @@ export default async function addStudent(formData){
   const sinav3 = formData.get('sinav3')
   const ort = (sinav1 + sinav2 + sinav3)/3;
   const name = formData.get('name')
-
+  console.log(formData);
+  
   const response = await fetch('https://edwrpskmitwxvipmcyoy.supabase.co/rest/v1/student',{
     method: "POST",
     headers: defaultHeader,
     body: JSON.stringify({
       name: name,
-      sinav1:  sinav1,
-      sinav2:  sinav2,
-      sinav3:  sinav3,
+      sinav_1:  sinav1,
+      sinav_2:  sinav2,
+      sinav_3:  sinav3,
       ort:  ort
     })
   })
+
+  if(response.ok){
+    console.log('response aldık')
+  }
 }
   
