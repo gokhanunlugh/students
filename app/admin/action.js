70d45f5198bab4cbd2028d7ab395e3d735e619a4
Gökhan Ünlü <gokhanunlu94@gmail.com>
2024-10-10 22:54:55 +0300

    ort ve name eklendi

--- app/admin/action.js
+++ app/admin/action.js
@@ -5,11 +5,14 @@ export default async function addStudent(formData){
   const sinav1 = formData.get('sinav1')
   const sinav2 = formData.get('sinav2')
   const sinav3 = formData.get('sinav3')
+  const ort = (sinav1 + sinav2 + sinav3)/3;
+  const name = formData.get('name')
 
   const response = await fetch('https://edwrpskmitwxvipmcyoy.supabase.co/rest/v1/student',{
     method: "POST",
     headers: defaultHeader,
     body: JSON.stringify({
+      name: name,
       sinav1:  sinav1,
       sinav2:  sinav2,
       sinav3:  sinav3,
